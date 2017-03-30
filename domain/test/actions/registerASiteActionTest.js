d8e2a1405b34334716d13801e7a1273d1c55af62
Nacho García <lupus.zgz@gmail.com>
2017-03-30 13:09:05 +0200

    Use let and actual objects

--- domain/test/actions/registerASiteActionTest.js
+++ domain/test/actions/registerASiteActionTest.js
@@ -1,12 +1,13 @@
 var actions = require('../../src/actions.js');
+var sites = require('../../src/model/Site.js');
 
 describe('Register a Site Action', () => {
   it('adds site to repository', () => {
-    var siteRepository = {put: () => {}};
+    let siteRepository = {put: () => {}};
     sinon.spy(siteRepository, 'put');
 
-    var site = {name:"A site"};
-    var registerASiteAction = new actions.RegisterASiteAction(siteRepository);
+    let site = new sites.Site("A site");
+    let registerASiteAction = new actions.RegisterASiteAction(siteRepository);
 
     registerASiteAction.run(site);
 

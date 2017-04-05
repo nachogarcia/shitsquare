2209b25020328bf5692923ceeb5812517ad49f05
Nacho García <lupus.zgz@gmail.com>
2017-04-05 14:35:37 +0200

    Change API to express

--- api/server.js
+++ api/server.js
@@ -1,33 +1,66 @@
-var rpc = require('json-rpc2');
-var Factory = require('../domain/src/Factory.js');
+var Factory = require('../domain/src/Factory.js')
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var factory = new Factory()
 
-let factory = new Factory();
+app.use(bodyParser.json());
 
-var server = rpc.Server.$create({
-    'websocket': true,
-    'headers': {
-        'Access-Control-Allow-Origin': '*'
-    }
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Content-Type', 'application/json');
+  next();
 });
+
+app.post('/api', function (req, res) {
+  console.log(req.body)
+  let method = req.body.method
+  let params = req.body.params
+  let id = req.body.id
+  let result = {}
+
+  switch (method) {
+    case 'registerASite':
+      result = registerASite(params)
+      break
+    case 'getClosest':
+      result = getClosest(params)
+      break
+    case 'registerAReview':
+      result = registerAReview(params)
+      break
+  }
+
+  let response = {
+    body: {
+      jsonrpc: "2.0",
+      result: result,
+      id: id
+    }
+  }
+  console.log(response)
+  res.send(response)
+})
+
+app.listen(8000, function () {
+  console.log('shitsquare-api listening on port 8000!')
+})
+
 
 function registerASite(siteData) {
   let registerASiteAction = factory.createRegisterASiteAction();
-  registerASiteAction.run(siteData);
+  return registerASiteAction.run(siteData);
 }
 
-function getClosest(coord, opt, callback) {
+function getClosest(coord) {
   let siteRepository = factory.createSiteRepository();
-  let sites = siteRepository.getClosest(coord,10);
-  callback(null, sites);
+  return siteRepository.getClosest(coord,10);
 }
 
-function registerAReview(args) {
+function registerAReview(params) {
+  let reviewData = params.reviewData
+  let siteData = params.siteData
   let registerAReviewAction = factory.createRegisterAReviewAction();
-  registerAReviewAction.run(args.reviewData, args.siteData.id);
+  return registerAReviewAction.run(args.reviewData, args.siteData.id);
 }
-
-server.expose('registerASite', registerASite);
-server.expose('getClosest', getClosest);
-server.expose('registerAReview', registerAReview);
-
-server.listen(8000, 'localhost');

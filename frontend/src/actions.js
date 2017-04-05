8e1bc15f68ad902ad646341aae0ade1d7c7d32f3
Nacho García <lupus.zgz@gmail.com>
2017-04-05 17:39:42 +0200

    Refactor actions

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -4,28 +4,21 @@ export function sendRegisterASite(siteData) {
   let method = 'registerASite'
   let id = '1'
 
-  let body = { method: method, params: siteData, id: id }
+  let body = { method, params: siteData, id }
 
-  let fetchData = {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }
-
-  return fetch(url, fetchData)
-  .then(response => response.json() );
+  return post(body)
 }
 
 export function sendGetClosest (coordinate) {
   let method = 'getClosest'
   let id = '2'
-  let params = { coordinate: coordinate }
 
-  let body = { method: method, params: params, id: id }
+  let body = { method, params: coordinate, id }
 
+  return post(body)
+  }
+
+function post (body) {
   let fetchData = {
     method: "POST",
     body: JSON.stringify(body),

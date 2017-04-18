d99d5f1b976b89ed4ca204a126cf086e4053cde1
Nacho García <lupus.zgz@gmail.com>
2017-04-18 10:33:38 +0200

    Add infoWindow again

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -1,11 +1,20 @@
 <template>
   <gmap-map :center="center" @center_changed="getClosestSites" :zoom="16" style="width: 100%; height:100%;">
+
+    <gmap-info-window
+      :options="infoOptions"
+      :position="infoWindowPosition"
+      :opened="infoWindowOpen"
+      :content="infoContent"
+      @closeclick="infoWindowOpen=false" />
+
     <gmap-marker
       v-for="site in sites"
       :key="site.id"
       :position="getMapCoordinates(site)"
       :clickable="true"
-      @click="displaySite(site)" />
+      @click="toggleInfoWindow(site)" />
+
   </gmap-map>
 </template>
 
@@ -22,20 +31,53 @@
 
   export default {
     data: () => ({
-      center: {lat: 0, lng: 0},
+      center: { lat: 0, lng: 0 },
       sites: [],
+      infoContent: '',
+      infoWindowPosition: {
+        lat: 0,
+        lng: 0
+      },
+      infoWindowOpen: false,
+      currentWindowId: null,
+      infoOptions: {
+        pixelOffset: {
+          width: 0,
+          height: -35
+        }
+      }
     }),
+
     methods: {
       getClosestSites: function () {
         sendGetClosest(this.getSiteCoordinates(this.center)).then((response) => {
           this.sites = response.body.result
         });
       },
+
       getMapCoordinates: function (site) { return {lat: site.coordinate.x, lng: site.coordinate.y}},
+
       getSiteCoordinates: function (marker) { return {x: marker.lat, y: marker.lng}},
-      displaySite: site => {console.log(site)},
+
+      displaySite: function (site) {
+        this.$router.push('/site');
+      },
+
+      toggleInfoWindow: function (site) {
+        this.infoWindowPosition = this.getMapCoordinates(site);
+        this.infoContent = site.name;
+        if (this.currentWindowId == site.id) {
+          this.infoWindowOpen = !this.infoWindowOpen;
+        }
+        else {
+          this.infoWindowOpen = true;
+          this.currentWindowId = site.id;
+        }
+      },
     },
+
     name: 'map',
+
     beforeMount() {
       let self = this;
       navigator.geolocation.getCurrentPosition((position) => {

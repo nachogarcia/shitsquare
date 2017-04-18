1e4360631da4f520a89b7e573ae80ae5e6683b6d
Nacho García <lupus.zgz@gmail.com>
2017-04-18 14:41:41 +0200

    Chain promises correctly

--- domain/test/repository/siteRepositoryTest.js
+++ domain/test/repository/siteRepositoryTest.js
@@ -2,6 +2,7 @@ var SiteRepository = require('../../src/model/SiteRepository.js');
 var actions = require('../../src/actions.js');
 var Factory = require('../../src/Factory.js');
 var Site = require('../../src/model/Site.js');
+var Review = require('../../src/model/Review.js');
 var Coordinate = require('../../src/model/Coordinate.js');
 var Migrator = require('../../src/infrastructure/Migrator.js');
 
@@ -13,10 +14,22 @@ describe('Site Repository', () => {
   beforeEach( () => {
     let factory = new Factory();
     let migrator = new Migrator(factory);
-    migrator.resetDB();
-
     siteRepository = factory.createSiteRepository();
-    site = new Site({id: siteRepository.nextSiteId(), name: "Test site", coordinate: new Coordinate(0,0)});
+    return migrator.resetDB().then((result) => {
+
+      let review = new Review({
+        id: siteRepository.nextReviewId(),
+        author: "An autor",
+        score: 5,
+        comment: "A comment",
+        time: "A time"});
+
+      site = new Site({
+        id: siteRepository.nextSiteId(),
+        name: "Test site",
+        coordinate: new Coordinate(0,0),
+        reviews:[review]});
+    });
   });
 
   it("generates next id's", () => {
@@ -25,21 +38,30 @@ describe('Site Repository', () => {
   });
 
   it('stores a site', () => {
-    siteRepository.put(site);
-
-    return siteRepository.findById(site.id).then( (storedSite) => {
-      expect(storedSite).to.deep.equal(site);
+    return siteRepository.put(site).then((result) => {
+      return siteRepository.findById(site.id).then( (storedSite) => {
+        expect(storedSite).to.deep.equal(site);
+      });
     });
   });
 
   it('updates a site', () => {
-    siteRepository.put(site).then((site) => {
+    return siteRepository.put(site).then((result) => {
       site.name = "changed name"
-      siteRepository.put(site);
-    });
 
-    return siteRepository.findById(site.id).then( (storedSite) => {
-      expect(storedSite).to.deep.equal(site);
+      let review = new Review({
+        id: siteRepository.nextReviewId(),
+        author: "Second Review",
+        score: 0,
+        comment: "Another comment",
+        time: "Another time"});
+
+      site.addReview(review);
+      return siteRepository.put(site);
+    }).then((result) => {
+      return siteRepository.findById(site.id).then( (storedSite) => {
+        expect(storedSite).to.deep.equal(site);
+      });
     });
   });
 
@@ -57,9 +79,23 @@ describe('Site Repository', () => {
     let farthestSite;
 
     beforeEach( () => {
+      let review = new Review({
+        id: siteRepository.nextReviewId(),
+        author: "Second Review",
+        score: 0,
+        comment: "Another comment",
+        time: "Another time"});
+
       currentPlace = new Coordinate(0,0);
-      closestSite = new Site({id: siteRepository.nextSiteId(), name: "Test site", coordinate: new Coordinate(1,1)});
+
+      closestSite = new Site({
+        id: siteRepository.nextSiteId(),
+        name: "Test Site 1",
+        coordinate: new Coordinate(1,1),
+        reviews: [review]});
+
       middleSite = new Site({id: siteRepository.nextSiteId(), name: "Test site", coordinate: new Coordinate(-3,10)});
+
       farthestSite = new Site({id: siteRepository.nextSiteId(), name: "Test site", coordinate: new Coordinate(100,-100)});
     });
 

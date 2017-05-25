572c889411c57a05bd9e2eafb5368d6a869edb70
Nacho Garcia <lupus.zgz@gmail.com>
2017-05-25 13:42:58 +0200

    Add production keys

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,3 +1,5 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  GMAP_API_KEY: '"AIzaSyB-tKE32_2l6mPFbIhZFIdHX06oZg2ukKs"',
+  API_URL: '"http://localhost:8000/api"'
 }

632f21dc6fb915e0f6808202f7794fb2864fbde8
Nacho Garcia <lupus.zgz@gmail.com>
2017-05-25 17:09:50 +0200

    Update production configuration in vue

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   GMAP_API_KEY: '"AIzaSyB-tKE32_2l6mPFbIhZFIdHX06oZg2ukKs"',
-  API_URL: '"http://localhost:8000/api"'
+  API_URL: '"http://api.shitsquare.es"'
 }
